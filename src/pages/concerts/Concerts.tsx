f3a9ea5b8abbda74e726f1f60216f819e43d3b08
Lucien Ramond <lucien_ramond@hotmail.com>
2025-11-06 13:00:51 +0100

    Update navbar and Concerts cards

--- src/pages/concerts/Concerts.tsx
+++ src/pages/concerts/Concerts.tsx
@@ -1,5 +1,4 @@
 import "react-day-picker/src/style.css";
-import { Separator } from "@/components/ui/separator";
 import { ArrowBigLeftIcon, ArrowBigRightIcon } from "lucide-react";
 import concerts from "./ConcertList";
 import {
@@ -19,51 +18,47 @@ export interface ConcertType {
 
 export default function Concerts() {
   return (
-    <div className="sm:px-4 mx-auto py-4 lg:max-w-[80%] sm:max-w-[90%] min-h-[75vh] sm:rounded bg-white shadow-lg">
-      <div className="w-full  p-2">
-        <div className="grid lg:grid-cols-2 gap-2 mx-auto ">
+    <div className="sm:px-4 mx-auto py-4 min-h-[73vh] bg-white/80 shadow-lg">
+      <div className="max-w-[1200px] mx-auto p-2">
+        <div className="grid sm:grid-cols-2 gap-2 sm:justify-center justify-center">
           {concerts &&
             concerts.map((concert: ConcertType) => {
               return (
                 <Card
                   id="concert-card"
                   key={concert.id}
-                  className={` bg-cover sm:h-[200px] xl:max-w-[600px] xl:mx-auto grid items-center hover:shadow-black border-black h-full w-full  text-white font-bold ${
+                  className={` p-2 bg-cover aspect-square max-h-[300px] min-w-[90vw] sm:min-w-full max-w-[500px] grid items-center hover:shadow-black border-black h-full  text-white font-bold ${
                     concert.id % 2 == 0
                       ? "bg-[url(/src/assets/icons/texture.jpg)] div-from-right"
-                      : "bg-[url(/src/assets/icons/texture2.jpg)] div-from-left"
+                      : "bg-[url(/src/assets/icons/texture2.jpg)] div-from-left text-right"
                   }`}
                 >
-                  <CardHeader className=" text-xl p-1 text-center bg-black/10">
+                  <CardHeader className=" text-xl p-1 italic bg-black/10">
                     {new Date(concert.date).toLocaleDateString("fr", {
                       day: "2-digit",
                       month: "long",
                       year: "numeric",
-                    })}{" "}
+                    })}
                   </CardHeader>
-                  <Separator className=" bg-white" />
                   <CardContent className=" p-2 ">
-                    <div className=" text-3xl text-center">
+                    <div className=" text-3xl ">
                       <div>{concert.event}</div>
                     </div>
-                    <div className="mx-auto italic sm:text-base text-xl text-center">
+                    <div className="mx-auto italic sm:text-base text-xl ">
                       ({concert.location})
                     </div>
                   </CardContent>
-                  <Separator className=" bg-white" />
-                  <CardFooter className="p-1 flex justify-center underline text-center bg-black/10">
+                  <CardFooter
+                    onClick={() =>
+                      concert.url && window.open(concert.url, "__blank")
+                    }
+                    className="cursor-pointer p-1 border rounded flex justify-center text-center bg-black/80"
+                  >
                     <ArrowBigRightIcon
                       fill="white"
                       className=" animate-pulse"
                     />
-                    <div
-                      className=" cursor-pointer "
-                      onClick={() =>
-                        concert.url && window.open(concert.url, "__blank")
-                      }
-                    >
-                      Plus d'infos ici
-                    </div>
+                    <div>Plus d'infos ici</div>
                     <ArrowBigLeftIcon fill="white" className=" animate-pulse" />
                   </CardFooter>
                 </Card>
--- src/pages/navbar/Navbar.tsx
+++ src/pages/navbar/Navbar.tsx
@@ -4,39 +4,49 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { Link } from "react-router-dom";
-import { Separator } from "@/components/ui/separator";
+import { Link, useLocation } from "react-router-dom";
+import Logo from "@/assets/logo-navbar.png";
 
 export default function Navbar() {
+  const location = useLocation();
+
   return (
-    <div className="flex text-white justify-around pl-4 pr-4 pt-4">
+    <div
+      className={`flex  text-white  justify-around pl-4 pr-4 pt-4 ${
+        location.pathname == "/" &&
+        "opacity-10 hover:opacity-100 transition duration-700 ease-in-out "
+      }`}
+    >
       <div className="flex items-center">
         <NavigationMenu>
           <NavigationMenuList>
-            <NavigationMenuItem>
-              <Link to={"/"} className={navigationMenuTriggerStyle()}>
-                Home
-              </Link>
-            </NavigationMenuItem>
-            <Separator orientation="vertical" className="h-4 bg-white" />
             <NavigationMenuItem>
               <Link to={"/le-groupe"} className={navigationMenuTriggerStyle()}>
                 Le groupe
               </Link>
             </NavigationMenuItem>
-            <Separator orientation="vertical" className="h-4 bg-white" />
             <NavigationMenuItem>
               <Link to={"/medias"} className={navigationMenuTriggerStyle()}>
                 Médias
               </Link>
             </NavigationMenuItem>
-            <Separator orientation="vertical" className="h-4 bg-white" />
+
+            <NavigationMenuItem>
+              <Link to={"/"}>
+                <img
+                  className=" hover:animate-pulse h-[120px]"
+                  src={Logo}
+                  alt="logo"
+                />
+              </Link>
+            </NavigationMenuItem>
+
             <NavigationMenuItem>
               <Link to={"/concerts"} className={navigationMenuTriggerStyle()}>
                 Concerts
               </Link>
             </NavigationMenuItem>
-            <Separator orientation="vertical" className="h-4 bg-white" />
+            {/* <Separator orientation="vertical" className="h-4 bg-white" /> */}
             <NavigationMenuItem>
               <Link to={"/contact"} className={navigationMenuTriggerStyle()}>
                 Contact
